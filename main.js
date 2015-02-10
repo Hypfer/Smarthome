b7dfea1aded52f873cbeb4c910a680a2c3bad8df
Hypfer <soeren@soerenbeye.de>
2015-02-11 00:50:45 +0100

    Simple webinterface with highcharts

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
  * Created by hypfer on 10.02.15.
  */
 var dgram = require('dgram');
+var express = require('express');
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var fs = require('fs');
 
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
@@ -24,9 +29,8 @@ var _setupClient = function () {
         var collection = db.collection(receivedMessage[0]);
 
         collection.insert([{
-            v1: receivedMessage[1],
-            v2: receivedMessage[2],
-            ts: new Date().toISOString()
+            v1: parseFloat(receivedMessage[1]),
+            ts: new Date()
         }], function(err) {
                 assert.equal(err, null);
             }
@@ -44,6 +48,83 @@ var _setupClient = function () {
     })
 };
 
+var _setupWeb = function () {
+    console.log("Starting Webserver...");
+    var graphTpl = fs.readFileSync("./views/graph.hbs");
+    var graphTemplate = hbs.compile(graphTpl.toString());
+
+    var app = express();
+    app.set('view engine', 'hbs');
+    console.log(__dirname);
+    app.use(express.static(__dirname + '/res'));
+
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }));
+
+
+    app.get('/', function (req,res) {
+
+        var sensors = [];
+        var collection = db.collection("Sensors");
+        collection.find().toArray(function(err,docs){
+                assert.equal(null,err);
+                docs.forEach(function(doc){
+                    sensors.push({
+                        body: graphTemplate({
+                            sensor: doc.sensor,
+                            title: doc.fN,
+                            type: doc.value
+                        })
+                });
+            });
+        });
+        res.render('index', {sensors: sensors});
+
+    });
+    app.get('/sensors', function(req,res) {
+        var collection = db.collection("Sensors");
+        collection.find().toArray(function(err,docs){
+            assert.equal(null, err);
+            res.send(docs);
+
+        });
+    });
+    app.get('/sensors/:id', function(req,res){
+        var collection = db.collection(req.params.id);
+        minutes = req.query.minutes ? req.query.minutes : 30;
+        collection.find({ts: { $gt: new Date(new Date() - 1000*60*minutes)}}).toArray(function(err,docs){
+            assert.equal(null, err);
+            var returnArray = [];
+            docs.forEach(function(doc){
+                returnArray.push({
+                    x: doc.ts.getTime(),
+                    y: doc.v1
+
+                });
+            });
+            res.send(_.sortBy(returnArray,"x"));
+
+        });
+
+
+    });
+
+    var server = app.listen(3001, function () {
+
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Example app listening at http://%s:%s', host, port);
+
+    });
+
+
+};
+
+
+
 var url = 'mongodb://localhost:27017/homecontrol';
 MongoClient.connect(url, function(err, _db) {
     assert.equal(null, err);
@@ -52,5 +133,6 @@ MongoClient.connect(url, function(err, _db) {
 
     console.log("Connected to MongoDB.");
 
-    _setupClient ();
+    _setupClient();
+    _setupWeb();
 });

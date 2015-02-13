198d18bc149adfff4bfdc0af96d0891d9b8a7c34
Kenan Sulayman <kenan@sly.mn>
2015-02-13 22:15:36 +0100

    Refactor local.js

--- local.js
+++ local.js
@@ -2,29 +2,37 @@
  * Created by hypfer on 11.02.15.
  */
 child_process = require("child_process");
-assert = require('assert');
+
+var isRunning;
+
+// automatic loop
+var _poll = function() {
+    var _next = function () {
+        setTimeout(_poll, 15E3)
+    };
+
+    child_process.exec("/usr/local/bin/airsensor -o -v", function(err, stdout) {
+        var collection = db.collection("AIRQW");
+
+        if (+stdout !== 0)
+            return collection.insert([{
+                v: parseFloat(stdout),
+                ts: new Date()
+            }], function(err) {
+                console.log("GOT ERROR:", err);
+
+                _next();
+            });
+
+        return _next();
+    })
+};
 
 module.exports = {
-
     local: function(db) {
+        if (isRunning) return console.trace("BUG: airsensor polling has already started.");
+        isRunning = true;
 
-        var _poll = function () {
-            child_process.exec("/usr/local/bin/airsensor -o -v", function(err, stdout) {
-
-                var collection = db.collection("AIRQW");
-                if(parseFloat(stdout) != 0) {
-
-                    collection.insert([{
-                            v: parseFloat(stdout),
-                            ts: new Date()
-                        }], function (err) {
-                            assert.equal(err, null);
-                        }
-                    );
-                }
-                setTimeout(_poll, 15E3)
-                })
-            };
         _poll();
     }
 };
